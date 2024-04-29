bed659e653a30bb329609174be9cbb7daf5a638e
Caio Mariano <caio.siiiilva@gmail.com>
2024-04-29 20:55:00 -0300

    feat: Adicionar imagem do botão de voltar às páginas de criar e editar contato e logo na página Home. Refazer o css das páginas Home, criar e editar contato.

--- src/Components/HeaderHome.jsx
+++ src/Components/HeaderHome.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./HeaderHome.css";
+import Logo from "../assets/contato-logo.svg";
 
 const HeaderHome = () => {
   return (
     <header>
       <div className="content">
         <div className="logo">
-          <h1>Logo</h1>
+          {/* <h1>Logo</h1> */}
+          <img src={Logo} style={{ width: "150px" }} />
         </div>
         <div className="btn-addContact">
           <Link to="/post">
-            <button>Adicionar contato</button>
+            <button>Adicionar +</button>
           </Link>
         </div>
       </div>
