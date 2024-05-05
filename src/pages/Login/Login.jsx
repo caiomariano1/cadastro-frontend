e1ee2736c6619090456826f21bbd0c3cca5b8061
Caio Mariano <caio.siiiilva@gmail.com>
2024-05-05 17:36:01 -0300

    feat: Refatorar formulário da página de login. Criar autenticação de login.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,6 @@
 import "./Login.css";
+import { useState, useEffect } from "react";
+import { useAuth } from "../../hooks/useAuth";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { Link, useNavigate } from "react-router-dom";
 import * as yup from "yup";
@@ -7,31 +9,56 @@ import Logo from "../../assets/contato-logo.svg";
 
 const Login = () => {
   const navigate = useNavigate();
-  const handleClickLogin = async (values) => {
-    console.log(values);
-    const valid = await validationLogin.isValid(values);
-    // if (valid) {
-    //   window.location.href = "https://localhost:7086/swagger/index.html";
-    // }
-    if (valid) {
-      navigate("home");
-    }
-  };
-  const validationLogin = yup.object().shape({
-    email: yup
-      .string()
-      .email("Não é um email")
-      .required("É necessário preencher este campo"),
-    password: yup
-      .string()
-      .min(6, "A senha deve ter pelo menos 6 caracteres")
-      .required("É necessário preencher este campo"),
-  });
 
-  const initialValues = {
-    email: "",
-    password: "",
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const { login, error: authError, loading } = useAuth("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError("");
+
+    const user = {
+      email,
+      password,
+    };
+
+    const response = await login(user);
+
+    console.log(response);
   };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
+
+  // const handleClickLogin = async (values) => {
+  //   console.log(values);
+  //   const valid = await validationLogin.isValid(values);
+  //   if (valid) {
+  //     navigate("home");
+  //   }
+  // };
+  // const validationLogin = yup.object().shape({
+  //   email: yup
+  //     .string()
+  //     .email("Não é um email")
+  //     .required("É necessário preencher este campo"),
+  //   password: yup
+  //     .string()
+  //     .min(6, "A senha deve ter pelo menos 6 caracteres")
+  //     .required("É necessário preencher este campo"),
+  // });
+
+  // const initialValues = {
+  //   email: "",
+  //   password: "",
+  // };
 
   return (
     <div className="main-conteiner">
@@ -59,7 +86,59 @@ const Login = () => {
           <h2 className="h2-header-login">
             Preencha os campos abaixo <br /> para efetuar o login
           </h2>
-          <Formik
+          <form className="login-form" onSubmit={handleSubmit}>
+            <label className="label-none">
+              <span className="span-label">Email</span>
+              <input
+                className="form-field"
+                type="email"
+                name="email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+            <label>
+              <span className="span-label">Senha</span>
+              <input
+                className="form-field"
+                type="password"
+                name="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            {!loading && (
+              <button className="btn-login" type="submit">
+                Entrar
+              </button>
+            )}
+            {loading && (
+              <button className="btn-login" disabled>
+                Aguarde...
+              </button>
+            )}
+            {error && <p className="error-auth">{error}</p>}
+          </form>
+          <p className="p-login">
+            Não possui uma conta?
+            {/* <a href="#">Cadastre-se</a> */}
+            <Link className="link-login" to="/register">
+              {" "}
+              Registre-se
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
+
+{
+  /*        <Formik
             initialValues={initialValues}
             onSubmit={handleClickLogin}
             validationSchema={validationLogin}
@@ -95,75 +174,9 @@ const Login = () => {
                 />
               </div>
               <button className="btn-login" type="submit">
-                {/* <a href="https://localhost:7086/swagger/index.html">Entrar</a> */}
+                {/* <a href="https://localhost:7086/swagger/index.html">Entrar</a> *}
                 Entrar
               </button>
             </Form>
-          </Formik>
-          <p className="p-login">
-            Não possui uma conta?
-            {/* <a href="#">Cadastre-se</a> */}
-            <Link className="link-login" to="/register">
-              {" "}
-              Registre-se
-            </Link>
-          </p>
-        </div>
-      </div>
-    </div>
-
-    // <div className="conteiner">
-    //   <h1>Login</h1>
-    //   <Formik
-    //     initialValues={initialValues}
-    //     onSubmit={handleClickLogin}
-    //     validationSchema={validationLogin}
-    //   >
-    //     <Form className="login-form">
-    //       <div className="login-form-group">
-    //         <label htmlFor="email">Email:</label>
-    //         <Field
-    //           id="email"
-    //           name="email"
-    //           className="form-field"
-    //           placeholder="Digite seu email..."
-    //         />
-    //         <ErrorMessage
-    //           component="span"
-    //           name="email"
-    //           className="form-error"
-    //         />
-    //       </div>
-
-    //       <div className="login-form-group">
-    //         <label htmlFor="password">Senha:</label>
-    //         <Field
-    //           id="password"
-    //           name="password"
-    //           className="form-field"
-    //           placeholder="Digite sua senha..."
-    //           autoComplete="off"
-    //         />
-    //         <ErrorMessage
-    //           component="span"
-    //           name="password"
-    //           className="form-error"
-    //         />
-    //       </div>
-
-    //       <button className="button" type="submit">
-    //         {/* <a href="https://localhost:7086/swagger/index.html">Entrar</a> */}
-    //         Entrar
-    //       </button>
-    //     </Form>
-    //   </Formik>
-    //   <p>
-    //     Não possui uma conta?
-    //     {/* <a href="#">Cadastre-se</a> */}
-    //     <Link to="/register"> Cadastre-se</Link>
-    //   </p>
-    // </div>
-  );
-};
-
-export default Login;
+          </Formik> */
+}
